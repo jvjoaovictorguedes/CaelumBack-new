a26304e67e98626cc3db82003b3dbed50d418da4
jvjoaovictorguedes <jvjoaovictorguedes@gmail.com>
2026-09-11 00:52:45 -0300

    feat: ajuste na rota do personagem com problema.

--- src/routes/characterRoutes.js
+++ src/routes/characterRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express");
+
 const characterController = require("../controllers/characterController");
+
 const router = express.Router();
 
 router
   .route("/")
   .post(characterController.createCharacter)
   .get(characterController.getAllCharacters);
-
-router
-  .route("/:id/experience")
-  .post(characterController.addExperience);
 
 router
   .route("/:id")
